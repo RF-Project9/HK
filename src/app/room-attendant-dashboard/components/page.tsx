66caec1a637d7a0f9c4e6cdfc30bd84be63d6e68
RF-Project9 <ferdiantocc12@gmail.com>
2026-07-14 15:52:52 +0700

    page.tsx

--- src/app/room-attendant-dashboard/components/page.tsx
+++ src/app/room-attendant-dashboard/components/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppLayout from '@/components/AppLayout';
+import AppLayout from '../layout';
 import RoomAttendantContent from './components/RoomAttendantContent';
 
 export default function RoomAttendantDashboardPage() {
